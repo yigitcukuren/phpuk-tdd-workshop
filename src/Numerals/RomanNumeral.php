090d6f1d96be74550d0849a0cc62ff1d503d8861
Yigit Cukuren <yigitcukuren@jotform.com>
2023-02-14 18:40:19 +0300

    arabic to numeral

--- src/Numerals/RomanNumeral.php
+++ src/Numerals/RomanNumeral.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace Braddle\PhpUk2023\Numerals;
+
+class RomanNumeral
+{
+    public static function convertFromArabic(int $number)
+    {
+        if ($number === 1) {
+            return 'I';
+        }
+
+        return 'V';
+    }
+}
--- test/Numerals/RomanNumeralTest.php
+++ test/Numerals/RomanNumeralTest.php
@@ -1,11 +1,19 @@
 <?php
 
+namespace Numerals;
+
+use Braddle\PhpUk2023\Numerals\RomanNumeral;
 use PHPUnit\Framework\TestCase;
 
 class RomanNumeralTest extends TestCase
 {
-    public function testHolding()
+    public function test1IsI()
     {
-        $this->assertTrue(true);
+        $this->assertEquals('I', RomanNumeral::convertFromArabic(1));
+    }
+
+    public function test5IsV()
+    {
+        $this->assertEquals('V', RomanNumeral::convertFromArabic(5));
     }
 }
